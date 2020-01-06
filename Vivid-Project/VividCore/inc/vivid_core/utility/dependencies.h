cdc9b0016fca55436203d981be6b8a3ff5cd149e
Mykola Morozov <mcmikecreations@gmail.com>
2020-01-06 18:09:08 +0200

    Changed injector to const

--- Vivid-Project/VividCore/inc/vivid_core/utility/dependencies.h
+++ Vivid-Project/VividCore/inc/vivid_core/utility/dependencies.h
@@ -23,12 +23,13 @@ public:
 	static dependencies& get_instance();
 	static auto& get_injector() noexcept
 	{
-		return boost::di::make_injector(
+		const auto injector = boost::di::make_injector(
 			boost::di::bind<app::context_controllers::context_controller>
 			.to<app::context_controllers::glfw_context_controller>()
 		,	boost::di::bind<app::window_controllers::window_controller>
 			.to<app::window_controllers::glfw_window_controller>()
 		);
+		return injector;
 	}
 };
 
